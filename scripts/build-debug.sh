d879f321bb1fd33d450a067d0f1315f654fa4732
Jon Thysell <thysell@gmail.com>
2021-03-04 09:39:03 -0800

    Add Linux to CI

--- scripts/build-debug.sh
+++ scripts/build-debug.sh
@@ -5,7 +5,7 @@ pushd $(dirname $0)/..
 mkdir -p build
 cd build
 
-cmake .. -DCMAKE_BUILD_TYPE=Debug
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=Debug/
 cmake --build . --config Debug
 
 popd
--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -5,7 +5,7 @@ pushd $(dirname $0)/..
 mkdir -p build
 cd build
 
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=Release/
 cmake --build . --config Release
 
 popd
